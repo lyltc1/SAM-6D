bee760903eca6249751454f16dcee7392f20792c
lyltc1 <870767645@qq.com>
2024-07-18 14:19:56 +0800

    update readme

--- SAM-6D/demo.sh
+++ SAM-6D/demo.sh
@@ -3,16 +3,16 @@ cd Render
 blenderproc run render_custom_templates.py --output_dir $OUTPUT_DIR --cad_path $CAD_PATH #--colorize True 
 
 
-# Run instance segmentation model
-export SEGMENTOR_MODEL=sam
+# # Run instance segmentation model
+# export SEGMENTOR_MODEL=sam
 
-cd ../Instance_Segmentation_Model
-python run_inference_custom.py --segmentor_model $SEGMENTOR_MODEL --output_dir $OUTPUT_DIR --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH
+# cd ../Instance_Segmentation_Model
+# python run_inference_custom.py --segmentor_model $SEGMENTOR_MODEL --output_dir $OUTPUT_DIR --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH
 
 
-# Run pose estimation model
-export SEG_PATH=$OUTPUT_DIR/sam6d_results/detection_ism.json
+# # Run pose estimation model
+# export SEG_PATH=$OUTPUT_DIR/sam6d_results/detection_ism.json
 
-cd ../Pose_Estimation_Model
-python run_inference_custom.py --output_dir $OUTPUT_DIR --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH --seg_path $SEG_PATH
+# cd ../Pose_Estimation_Model
+# python run_inference_custom.py --output_dir $OUTPUT_DIR --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH --seg_path $SEG_PATH
 
